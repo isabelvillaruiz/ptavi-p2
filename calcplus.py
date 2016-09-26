52304e815bd1ab219a0e64dee92cfa6a38b3e46f
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-09-26 10:34:26 +0200

    probando

--- calcplus.py
+++ calcplus.py
@@ -10,7 +10,7 @@ class calculadora():
 	def minus(self,num1,num2):
 		return num1-num2
 
-class CalculadoraHija():
+class CalculadoraHija(Calculadora): #Calculadora Hija hereda de Calculadora plus y minus
 
 	def multiply(self,num1,num2):
 		return num1 * num2	
@@ -19,28 +19,23 @@ class CalculadoraHija():
 		return num1 / num2
 
 
-if __name__ == "__main__":
-
-	
-	try:
-		operando1 = float(sys.argv[1])
-		operando2 = float(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+def Operaciones(num1,num2):
 
 	if sys.argv[2] == "suma":
 		result = calculadora().plus(operando1,operando2)
 	elif sys.argv[2] == "resta":
 		result = calculadora().minus(operando1,operando2)
 	elif sys.argv[2] == "multiplica":
-		result =  CalculadoraHija().multiply(operando1,operando2)
+		result =  calculadora().multiply(operando1,operando2)
 	elif sys.argv[2] == "divide" :
 		if operando2 == 0:
 			sys.exit('Division by zero is not allowed')
 		else:
-			result = CalculadoraHija().divide(operando1,operando2)
+			result = calculadora().divide(operando1,operando2)
 	else:
 		sys.exit('This operation is not allowed ')
+
+
 
 
 print(result)
